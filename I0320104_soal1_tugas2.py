e95fd9548c40a21d56204c43de58d5c172e7bec3
TruelyHafidz <80144853+TruelyHafidz@users.noreply.github.com>
2021-03-12 23:38:26 +0700

    Add files via upload

---
+++ I0320104_soal1_tugas2.py
@@ -0,0 +1,76 @@
+#Truely Nur Hafidz
+#I0320104
+#Kelas C
+#Soal 1
+
+#menampilkan informasi program
+print("Menghitung luas persegi panjang, luas lingkaran, dan luas kubus")
+
+print("1. Menghitung luas persegi panjang")
+
+#input nilai panjang dan lebar
+p = float(input("Masukkan nilai panjang: "))
+l = float(input("Masukkan nilai lebar: "))
+
+#menghitung luas persegi panjang
+L_Pp = p * l
+
+#menampilkan hasil perhitungan
+print("luas persegi panjang: ", L_Pp)
+
+print("2. Menghitung luas lingkaran")
+
+#input nilai jari-jari
+r = float(input("Masukkan nilai jari-jari: "))
+
+#menghitung luas lingkaran
+luas_lingkaran = 3.14 * (r ** 2)
+
+#menampilkan hasil perhitungan
+print("luas_lingkaran: ", luas_lingkaran)
+
+print("3. Menghitung luas kubus")
+
+#input nilai panjang rusuk
+s = float(input("masukkan nilai panjang rusuk: "))
+
+#menghitung luas kubus
+luas_kubus = 6 * (s ** 2)
+
+#menampilkan hasil perhitungan
+print("luas_kubus: ", luas_kubus)
+
+print("----")
+
+#menampilkan informasi program
+print("Konversi suhu (celcius ke farenheit dan reamur ke kelvin)")
+
+print("1. Menghitung konversi suhu (celcius ke farenheit")
+
+#input suhu dalam celcius
+C = float(input("Masukkan suhu (celcius): "))
+
+#menghitung konversi suhu ke farenheit
+F = 9 * C / 5 + 32
+
+#menampilkan hasil perhitungan
+print("farenheit; ", F)
+
+print("2. Menghitung konversi suhu (reamur ke kelvin")
+
+#input suhu dalam reamur
+R = float(input("Masukkan suhu (reamur): "))
+
+#menghitung konversi suhu ke kelvin
+K = 5 * R / 4 + 273
+
+#menampilkan hasil perhitungan
+print("kelvin: ", K)
+
+
+
+
+
+
+
+
---
+++ I0320104_soal2_tugas2.py
@@ -0,0 +1,55 @@
+#Truely Nur Hafidz
+#I0320104
+#Kelas C
+#Soal 2
+
+a = "H"
+b = "i"
+c = "!"
+nama = "Truely Nur Hafidz"
+jurusan = "Teknik Industri"
+universitas = "Universitas Sebelas Maret"
+angkatan = 2020
+tanggal_lahir = float(24)
+bulan_lahir = float(1)
+tahun_lahir = float(2001)
+hobi = "membaca, menulis, berenang, dan berpetualang"
+tinggi_badan = 155
+berat_badan = 48
+ukuran_sepatu = 39
+desa = "Kalibeji"
+RT = "02"
+RW = "05"
+kecamatan = "Sempor"
+kode_pos = 154471
+kabupaten = "Kebumen"
+provinsi = "Jawa Tengah"
+tanggal_sekarang = 13
+bulan_sekarang = 3
+tahun_sekarang = 2021
+d = "Terima"
+e = "kasih"
+p1 = a + b + c
+p2 = d + e
+p2 = "Terima kasih"
+pembuka = p1
+penutup = p2
+umur = float(((tanggal_sekarang - tanggal_lahir) + ((bulan_sekarang - bulan_lahir) * 30) + ((tahun_sekarang-tahun_lahir) * 365)))
+
+print(pembuka)
+print("Nama saya", nama)
+print("Saya adalah mahasiswa jurusan", jurusan, "di", universitas, "angkatan", angkatan)
+print("Saya berumur", umur, "Hari")
+print("Saya senang", hobi)
+print("Tinggi badan saya", tinggi_badan, "cm")
+print("Berat badan saya", berat_badan, "kg")
+print("Ukuran sepatu saya", ukuran_sepatu)
+print("Sekarang saya tinggal di", "Desa", desa, "RT", RT, "RW", RW, "Kecamatan", kecamatan, "Kabupaten", kabupaten, kode_pos, "Provinsi", provinsi)
+print(penutup)
+
+
+
+
+
+
+
